9b0d621f3ad8cd2b0f53f7f98172887326c2c158
Charlene <charlenee.huynh@gmail.com>
2026-05-12 12:12:26 +0200

    2

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+result = []
+
+
+def matrix_shape(matrix):
+    # Output: list of integers [row,columns, element]
+    if isinstance(matrix[0], list):
+        result.append(len(matrix))
+        matrix_shape(matrix[0])
+    return result
