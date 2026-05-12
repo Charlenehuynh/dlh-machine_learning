37f1f7398d18045aedfb723b37fbad3b5f065ca8
Charlene <charlenee.huynh@gmail.com>
2026-05-12 12:21:48 +0200

    2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-
-result = []
+"""Recursion"""
 
 
 def matrix_shape(matrix):
     # Output: list of integers [row,columns, element]
     if isinstance(matrix[0], list):
-        result.append(len(matrix))
-        matrix_shape(matrix[0])
-    return result
+        return [len(matrix)] + matrix_shape(matrix[0])
+    else:
+        return [len(matrix)]
+
+
+# mat1 = [[1, 2], [3, 4]]
+# print(matrix_shape(mat1))
+# mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
+#         [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
+# print(matrix_shape(mat2))
