7f88517e00962e8581798ae0c03c38b6dd4ab154
Charlene <charlenee.huynh@gmail.com>
2026-05-13 18:03:00 +0200

    6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Function that concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """Args: arr1, arr2 are lists of floats. Return new list"""
+    return arr1 + arr2
+
+
+# arr1 = [1, 2, 3, 4, 5]
+# arr2 = [6, 7, 8]
+# print(cat_arrays(arr1, arr2))
