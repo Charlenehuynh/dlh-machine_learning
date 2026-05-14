b7c5b629d322c62285b02b792991f2d4c53d3de8
Charlene <charlenee.huynh@gmail.com>
2026-05-14 16:39:00 +0200

    10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,16 @@
+"""calculate shapes of array"""
+
+
+def np_shape(matrix):
+    """input matrix, output tuple"""
+    return matrix.shape
+
+
+# import numpy as np
+# mat1 = np.array([1, 2, 3, 4, 5, 6])
+# mat2 = np.array([])
+# mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+#                  [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
+# print(np_shape(mat1))
+# print(np_shape(mat2))
+# print(np_shape(mat3))
