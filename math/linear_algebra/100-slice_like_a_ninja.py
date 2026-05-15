5fd448183efdc954b4f0c347f917ac3c9be58c20
Charlene <charlenee.huynh@gmail.com>
2026-05-15 18:28:01 +0200

    100

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -2,24 +2,25 @@
 """Slice matrix along specific axess"""
 
 
-
 def np_slice(matrix, axes={}):
     """Output new matrix"""
     result = matrix
-    for k, value in axes.items():
-        # if axes.key == 0: ## horizontal
-        if k == 0:
-            result = result[value[0]: value[1], :]
-        if k == 1:
-            result = result[ : , value[0] : value[1]]
-        else:
+    my_list = [slice(None)] * matrix.ndim
+    for key, value in axes.items():
+        my_list[key] = slice(*value)
     return result
-    return result
-    return result
-
 
     # else: #slice vertical
 
 
-mat1 = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
-print(np_slice(mat1, axes={0: (0, 1)}))
+# import numpy as np
+# np_slice = __import__('100-slice_like_a_ninja').np_slice
+
+# mat1 = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+# print(np_slice(mat1, axes={1: (1, 3)}))
+# print(mat1)
+# mat2 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+#                  [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]],
+#                  [[21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]])
+# print(np_slice(mat2, axes={0: (2,), 2: (None, None, -2)}))
+# print(mat2)
