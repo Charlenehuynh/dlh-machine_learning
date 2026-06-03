fa51386d5194ebfb51c8157365d988440f7504ad
Charlene <charlenee.huynh@gmail.com>
2026-06-03 15:16:04 +0200

    9

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+def poly_derivative(poly):
+    
---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+def summation_i_squared(n):
+    if type(n) is not int:
+        return None
+    sum = (n * (n + 1) * (2 * n + 1)) / 6
+    return sum
+
+
+# n = 5
+# print(summation_i_squared(n))
