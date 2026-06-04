2926760d8197e2b0e264f38fad2234f88cc6c2b1
charlene <charlenee.huynh@gmail.com>
2026-06-04 14:14:51 +0200

    0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,9 +2,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+''' plot y as a line graph '''
+
 
 def line():
-
+    ''' y should be plotted as a solid red line '''
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     plt.xlim(0, 10)
