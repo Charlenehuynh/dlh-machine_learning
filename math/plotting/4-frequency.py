67436cc7136fdfa327d6a041b718b4bb62819ae0
charlene <charlenee.huynh@gmail.com>
2026-06-04 15:36:16 +0200

    4

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -16,7 +16,8 @@ def frequency():
     plt.hist(student_grades, bins=np.arange(0, 110, 10), edgecolor='black')
     plt.xticks(np.arange(0, 110, 10))
     plt.xlim(0, 100)
+    plt.ylim(0, 30)
     plt.show()
 
 
-frequency()
+# frequency()
