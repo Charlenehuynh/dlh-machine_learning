9014f01636270a5644410a84aa4897cd170e438e
charlene <charlenee.huynh@gmail.com>
2026-06-13 12:59:07 +0200

    Commit

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python3
+''' Create a class Poisson that represents a poisson distribution: '''
+
 
 class Poisson:
-    def __init__(self, data=None, lambtha=1.):
-        self.data = data
-        self.lambtha = float(lambtha)
-        if data is None:
-            return None
-        if not isinstance(data, list):
-            raise TypeError("data must be a list")
-        if self.lambtha < 0 or lambtha == 0:
-            raise ValueError("lambtha must be a positive value")
-        if len(data) < 2:
-            raise ValueError("data must contain multiple values")
-        sum = 0
-        for i in data:
-            sum = sum + i
-            print(sum)
-        self.lambtha = sum / len(data)
+    ''' Poisson '''
 
-    def pmf(self, k):
-        if not isinstance(k, int):
-            k = int(k)
-        
+    def __init__(self, data=None, lambtha=1.):
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = sum(data) / len(data)
+
 # import numpy as np
 
 # np.random.seed(0)
