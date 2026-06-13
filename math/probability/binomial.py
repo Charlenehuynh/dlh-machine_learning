984ad5da9c92c235d9ea7aa1ed10a0233671993e
Charlene <charlenee.huynh@gmail.com>
2026-06-13 21:39:45 +0200

    10

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -14,10 +14,14 @@ class Binomial:
             self.p = p
             if n <= 0:
                 raise ValueError("n must be a positive value")
-            if p < 0 or p > 1:
+            if p <= 0 or p >= 1:
                 raise ValueError("p must be greater than 0 and less than 1")
         else:
             sum = 0
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data < 2):
+                raise ValueError("data must contain multiple values")
             # calculate mean
             for i in data:
                 sum += i
