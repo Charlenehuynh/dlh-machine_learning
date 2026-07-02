197b542fb8bd7a5f33c3c127185909a5bff064e6
Charlene <charlenee.huynh@gmail.com>
2026-07-03 00:07:32 +0700

    0

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -27,5 +27,6 @@ def likelihood(x, n, P):
     return likelihoods
 
 
-# P = np.linspace(0, 1, 11) # [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+# P = np.linspace(0, 1, 11) # [0.0, 0.1, 0.2, 
+# 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 # print(likelihood(26, 130, P))
