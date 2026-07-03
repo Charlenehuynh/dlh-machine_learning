e8cdd4f492412c51c5a1bc61795ea024cce5a7d0
Charlene <charlenee.huynh@gmail.com>
2026-07-03 16:05:09 +0700

    1

---
+++ math/bayesian_prob/1-intersection.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""calculates the likelihood of obtaining this data"""
+
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """hypothetical probabilities"""
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that " "is " "greater than or equal to 0"
+        )
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    factorial = np.math.factorial
+    n_choose_x = factorial(n) / (factorial(x) * factorial(n - x))
+
+    likelihoods = n_choose_x * (P**x) * ((1 - P) ** (n - x))
+
+    return likelihoods
+
+
+def intersection(x, n, P, Pr):
+    "Calculates the intersection of obtaining this data"
+    " with the various hypo probabilities"
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    if not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that is"
+                         " greater than or equal to 0")
+
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    if np.any(Pr < 0) or np.any(Pr > 1):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+
+    L = likelihood(x, n, P)
+
+    return L * Pr
+
+
+# P = np.linspace(0, 1, 11)
+# Pr = np.ones(11) / 11
+# print(intersection(26, 130, P, Pr))
