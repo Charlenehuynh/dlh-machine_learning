f0300fdc4f75fc2579cf8630d9fc71e2e0945896
Charlene <charlenee.huynh@gmail.com>
2026-07-04 19:49:08 +0700

    0

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -22,7 +22,8 @@ def mean_cov(X):
 
 
 # np.random.seed(0)
-# X = np.random.multivariate_normal([12, 30, 10], [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 10000)
+# X = np.random.multivariate_normal([12, 30, 10], [[36, -30, 15],
+# [-30, 100, -20], [15, -20, 25]], 10000)
 # mean, cov = mean_cov(X)
 # print(mean)
 # print(cov)
