9a429541a523b28f61276e9bb151ffdc867028b2
Charlene <charlenee.huynh@gmail.com>
2026-07-04 20:24:18 +0700

    4

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -47,13 +47,13 @@ class MultiNormal:
         return pdf_value
 
 
-np.random.seed(0)
-data = np.random.multivariate_normal(
-    [12, 30, 10], [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 10000
-).T
-mn = MultiNormal(data)
-x = np.random.multivariate_normal(
-    [12, 30, 10], [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 1
-).T
-print(x)
-print(mn.pdf(x))
+# np.random.seed(0)
+# data = np.random.multivariate_normal(
+#     [12, 30, 10], [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 10000
+# ).T
+# mn = MultiNormal(data)
+# x = np.random.multivariate_normal(
+#     [12, 30, 10], [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 1
+# ).T
+# print(x)
+# print(mn.pdf(x))
