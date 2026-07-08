f8f93f0dd5243ec42a4952dabb2621c8ca771619
Charlene <charlenee.huynh@gmail.com>
2026-07-08 21:13:11 +0700

    1

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
+"""Write a python script that creates a pd.DataFrame from a dictionary"""
 
 import pandas as pd
 
-data = {
-    'First': [0.0, 0.5, 1.0, 1.5],
-    'Second': ["one", "two", "three", "four"]
-    }
-df = pd.DataFrame(data, ['A', 'B', 'C', 'D'])
+data = {"First": [0.0, 0.5, 1.0, 1.5],
+        "Second": ["one", "two", "three", "four"]}
+df = pd.DataFrame(data, ["A", "B", "C", "D"])
