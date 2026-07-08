a16c2a6401917fc396f2bcba7760198edf6c2270
Charlene <charlenee.huynh@gmail.com>
2026-07-08 20:51:48 +0700

    0

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-import string
-import numpy as np
 import pandas as pd
 
 
 def from_numpy(array):
     size = array.shape
-    column = list(string.ascii_uppercase[: size[1]])
+    abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    column = list(abc[: size[1]])
     df = pd.DataFrame(array, columns=column)
     return df
 
