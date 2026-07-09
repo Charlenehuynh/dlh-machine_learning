e78619bde26d619dc67550e69382bca8ba711f94
Charlene <charlenee.huynh@gmail.com>
2026-07-09 21:29:17 +0700

    11

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -3,9 +3,11 @@
 
 import pandas as pd
 
+index = __import__("10-index").index
+
 
 def concat(df1, df2):
-    index = __import__("10-index").index
+    """Concat"""
     df1 = index(df1)
     df2 = index(df2)
     df2 = df2[df2.index <= 1417411920]
