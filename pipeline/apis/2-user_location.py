cec35250504185685aa261bcb2c00864e14c7e0c
Charlene <charlenee.huynh@gmail.com>
2026-07-22 15:59:38 +0200

    2

---
+++ pipeline/apis/2-user_location.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+import requests
+import time
+
+
+def get_user_location():
+    if len(sys.argv) < 2:
+        return
+
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        json_data = response.json()
+        location = json_data.get("location")
+        if location:
+            print(location)
+        else:
+            print("No location")
+    elif response.status_code == 404:
+        print("Not found")
+    elif response.status_code == 403:
+        reset_timestamp = int(response.headers.get("X-Ratelimit-Reset", 0))
+        current_timestamp = int(time.time())
+
+        reset_seconds = reset_timestamp - current_timestamp
+        reset_minutes = int(reset_seconds / 60)
+        print(f"Reset in {reset_minutes} min")
+    if __name__ == "__main__":
+        get_user_location()
